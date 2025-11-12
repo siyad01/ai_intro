8e36fc622de8e515e206e632434206c4265bef21
Muhammed Siyad <144800182+siyad01@users.noreply.github.com>
2025-11-12 14:49:09 +0530

    YoloV8 Quickstart

---
+++ detect.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO('yolov8n.pt')
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    results = model(frame)
+
+    annotated_frame = results[0].plot()
+    cv2.imshow('YOLOv8 Live Detection', annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+results[0].save('results.jpg')
+cap.release()
+cv2.destroyAllWindows()
